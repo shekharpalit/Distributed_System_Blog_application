068bdfe40ae0dc37cbe09e1df13a5eef71c7884f
shekharpalit <shekharpalit222@gmail.com>
2019-10-02 22:36:35 -0700

    fixed adapter

--- adapter.rb
+++ adapter.rb
@@ -12,17 +12,43 @@ server.add_handler '_in' do |array_values|
     array_values.each do |t|
       ts.write(t)
     end
+    message = "done with data insertion\n"
+    String(message)
 end
 
 
 server.add_handler '_rd' do |value_to_matched|
-    res = ts.read([value_to_matched[0], value_to_matched[1], String])
+    values = []
+    Array(value_to_deleted).each do |t|
+        if t['class'] == "String"
+            values.push(String)
+        elsif t['class'] == 'Numeric'
+            values.push(Numeric)
+        elsif t['regexp'] ==  '^[-+/*]$'
+            values.push(%r{^[-+/*]$})
+        else
+            values.push(t)
+        end
+    end
+    res = ts.read(values)
     String(res)
 end
 
 
 server.add_handler '_out' do |value_to_deleted|
-    res = ts.take([value_to_deleted[0], value_to_deleted[1], String])
+    values = []
+    Array(value_to_deleted).each do |t|
+        if t['class'] == "String"
+            values.push(String)
+        elsif t['class'] == 'Numeric'
+            values.push(Numeric)
+        elsif t['regexp'] ==  '^[-+/*]$'
+            values.push(%r{^[-+/*]$})
+        else
+            values.push(t)
+        end
+    end
+    res = ts.take(values)
     String(res)
 end
 
